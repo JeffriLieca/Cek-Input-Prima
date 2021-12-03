37b81f23547cefb73a4781371c7cae8ab2ad11aa
JeffriLieca <jhandoyo@student.ciputra.ac.id>
2021-12-03 10:23:22 +0700

    Mengecek jumlah counter

--- Cek Input Prima/Program.cs
+++ Cek Input Prima/Program.cs
@@ -9,6 +9,14 @@ namespace Cek_Input_Prima
             Console.WriteLine("Cek Input apakah Bilangan Prima atau Bukan");
             Console.Write("Input :");
             int Input = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+            for(int i = 1; i <= Input; i++)
+            {
+                if (Input % i == 0)
+                {
+                    counter++;
+                }
+            }
         }
     }
 }
