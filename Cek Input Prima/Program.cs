7c3c3cbc6e552d72c8568526b81ed6f61caf4f37
JeffriLieca <jhandoyo@student.ciputra.ac.id>
2021-12-03 10:21:02 +0700

    Meminta Input

--- Cek Input Prima/Program.cs
+++ Cek Input Prima/Program.cs
@@ -7,6 +7,8 @@ namespace Cek_Input_Prima
         static void Main(string[] args)
         {
             Console.WriteLine("Cek Input apakah Bilangan Prima atau Bukan");
+            Console.Write("Input :");
+            int Input = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
